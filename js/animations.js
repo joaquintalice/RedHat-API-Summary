aa41a5f0349356d9344b11be0429ae39dda4e94e
Joaquin Talice <joaquintalice@gmail.com>
2023-09-02 21:49:40 -0300

    feat: fade animation to entire page

---
+++ js/animations.js
@@ -0,0 +1,32 @@
+const elementosDiv = document.querySelectorAll('div').forEach(elem => {
+    elem.classList.add('hiddenElem')
+});
+const elementosH3 = document.querySelectorAll('h3').forEach(elem => {
+    elem.classList.add('hiddenElem')
+});
+const elementosH4 = document.querySelectorAll('h4').forEach(elem => {
+    elem.classList.add('hiddenElem')
+});
+const elementosP = document.querySelectorAll('p').forEach(elem => {
+    elem.classList.add('hiddenElem')
+});
+
+const header = document.getElementById('header').classList.remove('hiddenElem');
+const nav = document.getElementById('header').classList.remove('nav-bar');
+
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showElem');
+        } else {
+            entry.target.classList.remove('showElem');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hiddenElem');
+hiddenElements.forEach((el) => observer.observe(el));
